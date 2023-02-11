c089b92a2b535f36e495e8c804ea5ffec3b24fa8
Dell <Dell@DESKTOP-1JN7CA4>
2023-02-11 22:12:32 +0530

    added citizentype testclass

---
+++ git_practice/src/com/git/testcases/CitizenType.java
@@ -0,0 +1,17 @@
+package com.git.testcases;
+
+public class CitizenType {
+
+	public static void main(String[] args) {
+		int citizenAge=15;
+		if(citizenAge>18)
+		{
+			System.out.println("he is eligible for voting");
+		}
+		else
+		{
+			System.out.println("he is not eligible for voting");
+		}
+	}
+
+}
