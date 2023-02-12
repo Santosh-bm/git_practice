2d82879f9d8ae3283bc0be9191c2565083ecbebb
Dell <Dell@DESKTOP-1JN7CA4>
2023-02-12 16:14:14 +0530

    User id validation added

--- git_practice/src/com/git/testcases/StringFunction.java
+++ git_practice/src/com/git/testcases/StringFunction.java
@@ -3,8 +3,17 @@ package com.git.testcases;
 public class StringFunction {
 
 	public static void main(String[] args) {
-		String str="annu";
-		System.out.println(str.length());
+		String str="anuradha";
+		//System.out.println(str.length());
+		//int s=str.length();
+		if(str.length()>=5)
+		{
+			System.out.println("given user id is valid");
+		}
+		else
+		{
+			System.out.println("given user id is invalid");
+		}
 
 	}
 
